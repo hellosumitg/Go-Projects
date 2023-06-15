65690511596caf93ffbe1fc1722c5b7f1d08fe8e
Sumit Gupta <hellosumitg@yahoo.com>
2023-06-15 13:58:19 +0530

    Day6

--- crypto-terminal-indicator/main.go
+++ crypto-terminal-indicator/main.go
@@ -51,7 +51,7 @@ type Orderbook struct {
 	Bids map[float64]float64
 }
 
-// Function to create a new orderbook
+// `NewOrderbook()` is a constructor function for creating a new insatnce of `Orderbook` struct
 func NewOrderbook() *Orderbook {
 	return &Orderbook{
 		Asks: make(map[float64]float64),
@@ -59,7 +59,10 @@ func NewOrderbook() *Orderbook {
 	}
 }
 
-// Function to handle depth response for the orderbook
+// Function to handle depth response for the received from the Binance API so as to maintain the orderbook
+// It takes two slices, asks and bids, as input. It iterates over the asks and bids slices,
+// parses the price and volume values from each entry,
+// and calls the addAsk() and addBid() methods to update the order book accordingly.
 func (ob *Orderbook) handleDepthResponse(asks, bids []interface{}) {
 	for _, v := range asks {
 		ask := v.([]interface{})
@@ -78,6 +81,8 @@ func (ob *Orderbook) handleDepthResponse(asks, bids []interface{}) {
 // Function to add a bid to the orderbook
 func (ob *Orderbook) addBid(price, volume float64) {
 	if _, ok := ob.Bids[price]; ok {
+		// If the volume is zero, the entry is deleted from the respective map.
+		// Otherwise, it adds or updates the volume for the given price.
 		if volume == 0.0 {
 			delete(ob.Bids, price)
 			return
@@ -88,28 +93,31 @@ func (ob *Orderbook) addBid(price, volume float64) {
 
 // Function to add an ask to the orderbook
 func (ob *Orderbook) addAsk(price, volume float64) {
-	if volume == 0 {
+	if volume == 0.0 {
 		delete(ob.Asks, price)
 		return
 	}
 	ob.Asks[price] = volume
 }
 
-// Function to get the best bids from the orderbook
+// The two methods `getBids()` and `getAsks()`, Function to get the best `bids` and `asks` entries from the orderbook
 func (ob *Orderbook) getBids() []OrderbookEntry {
 	depth := 10
-	entries := make(byBestBid, len(ob.Bids))
+	entries := make(byBestBid, len(ob.Bids)) // creates a slice entries of type byBestBid (custom sorting interface) with a length equal to the number of bids in the Bids map.
 	i := 0
+	// It iterates over the Bids map and filters out entries with zero volume.
 	for price, volume := range ob.Bids {
 		if volume == 0 {
 			continue
 		}
+		// Then, it assigns the price and volume values to the corresponding OrderbookEntry struct and adds it to the entries slice.
 		entries[i] = OrderbookEntry{
 			Price:  price,
 			Volume: volume,
 		}
 		i++
 	}
+	//  Finally, it sorts the entries slice and returns a portion of it based on the depth value (in this case, 10).
 	sort.Sort(entries)
 	if len(entries) >= depth {
 		return entries[:depth]
@@ -143,7 +151,7 @@ type BinanceTradeResult struct {
 	} `json:"data"`
 }
 
-// Structure representing the depth result from Binance
+// Structure representing the depth result from Binance API
 type BinanceDepthResult struct {
 	Asks [][]string `json:"a"`
 	Bids [][]string `json:"b"`
@@ -160,26 +168,33 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// This code establishes a WebSocket connection to the Binance API using the github.com/gorilla/websocket
 	conn, _, err := websocket.DefaultDialer.Dial(wsendpoint, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	var (
 		ob     = NewOrderbook()
-		result map[string]interface{}
+		result map[string]interface{} // `result` variable will be used to store the JSON response from the WebSocket connection.
 	)
+
 	go func() {
+		// goroutine to continuously read JSON messages from the WebSocket connection using the `conn.ReadJSON()` method.
+		// It parses the received JSON into the `result` map
 		for {
 			if err := conn.ReadJSON(&result); err != nil {
 				log.Fatal(err)
 			}
-			stream := result["stream"].(string)
+			// below `stream` variable denotes the `stream` field in the received JSON to determine if it is a depth update or a mark price update.
+			stream := result["stream"].(string) 
+			// If it is a depth update, it extracts the asks and bids data from the result map and calls the ob.handleDepthResponse() method to update the order book.
 			if stream == "btcusdt@depth" {
 				data := result["data"].(map[string]interface{})
 				asks := data["a"].([]interface{})
 				bids := data["b"].([]interface{})
 				ob.handleDepthResponse(asks, bids)
 			}
+			// If it is a marketprice update, it updates the previous mark price, current mark price, and funding rate variables.
 			if stream == "btcusdt@markPrice" {
 				prevMarkPrice = currMarkPrice
 				data := result["data"].(map[string]interface{})
